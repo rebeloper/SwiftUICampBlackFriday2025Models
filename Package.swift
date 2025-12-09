423843d25b462d80b1874d45fc98751ef4c218e6
rebeloper <therebeloper@gmail.com>
2025-12-09 17:59:06 +0200

    first commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUICampBlackFriday2025Models",
+    platforms: [
+        .iOS(.v17), .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,12 +15,19 @@ let package = Package(
             targets: ["SwiftUICampBlackFriday2025Models"]
         ),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: Version(12, 6, 0))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUICampBlackFriday2025Models"
-        ),
+            name: "SwiftUICampBlackFriday2025Models",
+            dependencies: [
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+            ]),
 
     ]
 )
